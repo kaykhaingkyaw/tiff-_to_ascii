c90bed9a4dbe217d5056091dc11a6c99910dc77e
kaykhaingkyaw <iamkaykhaingkyaw@gmail.com>
2023-12-05 14:53:38 +0100

    Update tiff_to_ascii.R

--- tiff_to_ascii.R
+++ tiff_to_ascii.R
@@ -9,8 +9,6 @@ tiff_file <- "C:/Users/iamka/OneDrive/Documents/DICAM/Catania/02DEM/lidar_cata.D
 # Read the TIFF file
 tif_data <- readTIFF(tiff_file)
 
-
-
 # Read the TIFF file using raster::raster
 raster_data <- raster(tiff_file)
 
@@ -20,11 +18,8 @@ raster_values <- getValues(raster_data)
 # Combine coordinates and values
 ascii_data <- cbind(coordinates(raster_data), raster_values)
 
-
 # Specify the output ASCII file with a .asc extension
 output_file <- "dem.asc"
-
-
 
 # Save ASCII data to a text file with .asc extension
 write.table(ascii_data, output_file, col.names = FALSE, row.names = FALSE, quote = FALSE)
